eb9480375bc42afc18b90a9cf16457befe321c73
Вишняков Юрий <ysnp15@yandex.ru>
2026-05-20 20:45:44 +0300

    Calculator рефакторинг

---
+++ Calculator/Enums/MenuOption.cs
@@ -0,0 +1,8 @@
+﻿namespace Calculator.Enums;
+
+enum MenuOption
+{
+    InvalidOption = 0,
+    Calculator = 1,
+    Exit = 2
+}
--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,163 +1,122 @@
-﻿bool work = true;
-List<string> menuOptions = [
-    "1 Калькулятор",
-    "2 Выход"
+﻿using Calculator.Enums;
+
+namespace Calculator;
+
+public class Program
+{
+    private static bool _isWorking = true;
+    private static List<string> _menuOptions =
+    [
+        "1 Калькулятор",
+        "2 Выход"
     ];
 
-while (work)
-{
-    PrintMenu();
-    string option = Console.ReadLine();
-    OptionalHandleResult result = HandleOption(option);
-    Console.WriteLine(HandleResult(result));
-}
-
-void PrintMenu()
-{
-    foreach (string option in menuOptions)
+    public static void Main( string[] args )
     {
-        Console.WriteLine(option);
+        while ( _isWorking )
+        {
+            PrintMenu();
+            string option = Console.ReadLine();
+            OptionalHandleResult result = HandleOption( option );
+            string handledResult = HandleResult( result );
+            if ( handledResult != null )
+            {
+                Console.WriteLine( handledResult );
+            }
+        }
     }
-    Console.WriteLine();
-}
-
-OptionalHandleResult HandleOption(string option)
-{
-    switch (option)
+    
+    private static void PrintMenu()
     {
-        case "1":
-            return Calculate();
-        case "2":
-            return Exit();
-        default:
-            return OptionalHandleResult.InvalidOption;
-    }
-}
-
-OptionalHandleResult Calculate()
-{
-    Console.WriteLine("Введите выражение: {число} действие {число}.");
-    Console.WriteLine("Доступные действия: +, -, *, /");
-    Console.WriteLine();
-
-    int firstOperand;
-    int secondOperand;
-    Action action;
-    try
-    {
-        string[] arr = ReadInput();
-        firstOperand = ParseOperand(arr[0]);
-        action = ParseAction(arr[1]);
-        secondOperand = ParseOperand(arr[2]);
-
-        if (secondOperand == 0 && action == Action.Divide)
-            return OptionalHandleResult.InvalidInput;
-    }
-    catch (Exception ex)
-    {
-        return OptionalHandleResult.InvalidInput;
-    }
-
-    try
-    {
-        int result = HandleAction(firstOperand, action, secondOperand);
-        Console.WriteLine($"Результат: {result}");
+        foreach ( string option in _menuOptions )
+        {
+            Console.WriteLine( option );
+        }
         Console.WriteLine();
     }
-    catch
+
+    private static OptionalHandleResult HandleOption( string option )
     {
-        return OptionalHandleResult.Overflow;
-    }
-    return OptionalHandleResult.Success;
-}
+        if ( !Enum.TryParse( option, out MenuOption menuOption ) )
+        {
+            return OptionalHandleResult.InvalidOption;
+        }
 
-string[] ReadInput()
-{
-    string input = Console.ReadLine();
-    string[] inputArr = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
-    if (inputArr.Length != 3)
+        switch ( menuOption )
+        {
+            case MenuOption.Calculator:
+                return Calculate();
+            case MenuOption.Exit:
+                return Exit();
+            default:
+                return OptionalHandleResult.InvalidOption;
+        }
+    }
+
+    private static OptionalHandleResult Calculate()
     {
-        throw new Exception();
+        Console.WriteLine( "Введите выражение: {число} действие {число}." );
+        Console.WriteLine( "Доступные действия: +, -, *, /" );
+        Console.WriteLine();
+
+        int firstOperand = 0;
+        int secondOperand = 0;
+        MathAction action = MathAction.InvalidAction;
+
+        Calculator calculator = new Calculator();
+
+        string[] arr = ReadInput();
+
+        if ( !calculator.TryParseInput( arr, out firstOperand, out action, out secondOperand ) )
+        {
+            return OptionalHandleResult.InvalidInput;
+        }
+
+        if ( calculator.IsOverflow( firstOperand, secondOperand ) )
+        {
+            return OptionalHandleResult.Overflow;
+        }
+
+        if ( !calculator.TryHandleAction( firstOperand, action, secondOperand, out int result ) )
+        {
+            return OptionalHandleResult.InvalidInput;
+        }
+
+        Console.WriteLine( $"Результат: {result}" );
+        Console.WriteLine();
+
+        return OptionalHandleResult.Success;
     }
-    return inputArr;
-}
 
-int ParseOperand(string input)
-{
-    if (!int.TryParse(input, out int intOperand))
-        throw new Exception();
-    return intOperand;
-}
-
-Action ParseAction(string input)
-{
-    switch (input)
+    private static string[] ReadInput()
     {
-        case "+":
-            return Action.Add;
-        case "-":
-            return Action.Subtract;
-        case "*":
-            return Action.Multiply;
-        case "/":
-            return Action.Divide;
-        default:
-            throw new Exception();
-    }
-}
+        string input = Console.ReadLine();
+        string[] inputArr = input.Split( " ", StringSplitOptions.RemoveEmptyEntries );
 
-int HandleAction(int firstOperand, Action action, int secondOperand)
-{
-    switch (action)
+        return inputArr;
+    }
+
+    private static OptionalHandleResult Exit()
     {
-        case Action.Add:
-            return firstOperand + secondOperand;
-        case Action.Subtract:
-            return firstOperand - secondOperand;
-        case Action.Multiply:
-            return firstOperand * secondOperand;
-        case Action.Divide:
-            return firstOperand / secondOperand;
-        default:
-            throw new Exception();
+        _isWorking = false;
+
+        return OptionalHandleResult.Success;
     }
-}
 
-OptionalHandleResult Exit()
-{
-    work = false;
-    return OptionalHandleResult.Success;
-}
-
-string HandleResult(OptionalHandleResult result)
-{
-    switch (result)
+    private static string HandleResult( OptionalHandleResult result )
     {
-        case OptionalHandleResult.Success:
-            return "";
-        case OptionalHandleResult.InvalidOption:
-            return "Неверная команда";
-        case OptionalHandleResult.InvalidInput:
-            return "Неверно введеные данные";
-        case OptionalHandleResult.Overflow:
-            return "Ошибка переполнения";
-        default:
-            return "Критическая ошибка";
-    }
-}
-
-enum OptionalHandleResult
-{
-    Success = 0,
-    InvalidOption = 1,
-    InvalidInput = 2,
-    Overflow = 3
-}
-
-enum Action
-{
-    Add,
-    Subtract,
-    Multiply,
-    Divide
+        switch ( result )
+        {
+            case OptionalHandleResult.Success:
+                return null;
+            case OptionalHandleResult.InvalidOption:
+                return "Неверная команда";
+            case OptionalHandleResult.InvalidInput:
+                return "Неверно введеные данные";
+            case OptionalHandleResult.Overflow:
+                return "Ошибка переполнения";
+            default:
+                return "Критическая ошибка";
+        }
+    }  
 }
